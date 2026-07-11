33e23856547e4a818bc8a6d23396642501400274
Rasmus Stenlund <rasmus.stenlund@gmail.com>
2026-07-11 22:57:46 +0200

    Add delete endpoint and basic scripts for website with header links

--- app/main.py
+++ app/main.py
@@ -80,6 +80,19 @@ def update_experiment(experiment_id: int, experiment: ExperimentUpdate):
         session.commit()
         session.refresh(db_experiment)
         return db_experiment
+    
+@app.delete("/experiments{experiment_id}")
+def delete_experiment(experiment_id: int):
+    with Session(engine) as session:
+        experiment = session.get(Experiment, experiment_id)
+        if not experiment:
+            raise HTTPException(
+                status_code = 404,
+                detail = "Experiment not found"
+            )
+        session.delete(experiment)
+        session.commit()
+        return {"ok": True}
 
 
 @app.get("/experiments", response_model = list[ExperimentPublic])
